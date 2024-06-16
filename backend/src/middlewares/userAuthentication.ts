2e0d1bf71d2b23aa1fe54a35988fd02ed00f5225
Rithk <rithkchaudhary86@gmail.com>
2024-06-16 14:03:01 +0530

    middleware

--- backend/src/middlewares/userAuthentication.ts
+++ backend/src/middlewares/userAuthentication.ts
@@ -1,20 +1,28 @@
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config";
 import { Request,Response,NextFunction } from "express";
+import { CustomRequest } from "../types/CustomRequest";
 
 interface DecodedToken{
     userId: string
 }
-export const userAuth = (req: Request,res: Response,next: NextFunction)=>{
+export const userAuth = (req: CustomRequest,res: Response,next: NextFunction)=>{
     const token: any = req.headers.authorization;
     if(!token){
         return res.status(403).json({
             message: "you are not authorized"
         })
     }
-    const decode : DecodedToken = jwt.verify(token,JWT_SECRET) as DecodedToken;
-    const userId = decode.userId;
-    next();
+    try {
+        const decode : DecodedToken = jwt.verify(token,JWT_SECRET) as DecodedToken;
+        
+        req.userId = decode.userId;
+        next();
+    } catch (error) {
+        return res.status(403).json({
+            message: "You are not authorized"
+        })
+    }
 
 
 }
